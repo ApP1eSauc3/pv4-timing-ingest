e9baff7fd4f42666adc8ccbd9e6f6b6b3025d953
Liam Howe <liamhowe2644@gmail.com>
2026-09-20 20:30:45 +1000

    Initial commit

---
+++ test/candidate_brief_test.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CandidateBriefTest from '../lib/candidate_brief_test-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/candidate_brief_test-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CandidateBriefTest.CandidateBriefTestStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
